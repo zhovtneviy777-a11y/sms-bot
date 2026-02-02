cead21130fff18464c591707f561005ebfe1a283
zhovtneviy777-a11y <zhovtneviy777@gmail.com>
2026-02-02 13:31:58 +0300

    Initialize Flask SMS Bot with basic route

---
+++ bot.py
@@ -0,0 +1,16 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return '''
+    <h1>🤖 SMS Bot працює!</h1>
+    <p>Вітаю! Бот успішно запущено на Render.</p>
+    <p>Наступний крок - налаштування Telegram.</p>
+    '''
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
